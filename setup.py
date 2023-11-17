1e54eb0c8c67f15865c3f44712989badced5b5e2
catpineapple <1391869588@qq.com>
2023-11-17 18:34:02 +0800

    adjustment adapter name from selectdb to doris

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 from setuptools import find_namespace_packages, setup
 
-package_name = "dbt-selectdb"
+package_name = "dbt-doris"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "0.1.0"
-dbt_core_version = "1.3.0"
-description = """The selectdb cloud adapter plugin for dbt, Original code fork from Apache Doris"""
+package_version = "0.3.4"
+dbt_core_version = "1.5.0"
+description = """The doris adapter plugin for dbt """
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
     long_description=description,
-    author="catpineapple,JNSimba",
+    author="long2ice,catpineapple,JNSimba",
     author_email="1391869588@qq.com",
-    url="https://github.com/selectdb/dbt-selectdb.git",
+    url="https://github.com/selectdb/dbt-doris",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
         "dbt-core~={}".format(dbt_core_version),
         "mysql-connector-python>=8.0.0,<8.1",
+        "urllib3~=1.0",
     ],
-    python_requires=">=3.8,<=3.10",
+    python_requires=">=3.7.2",
 )
